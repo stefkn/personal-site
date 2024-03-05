b83a353c4742fd5b713c60342d18be5c2ce8fb5b
stefkn <stefan.nowak@tails.com>
2024-03-05 03:05:19 +0000

    add a schema for the postsCollection

---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const postsCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    description: z.string(),
+    author: z.string(),
+    image: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    imageAlt: z.string(),
+    tags: z.array(z.string())
+  })
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  posts: postsCollection,
+};
