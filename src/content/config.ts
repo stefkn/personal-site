272af10992fad2da4a9a6575f00275d99c95d38d
stefkn <stefan.nowak@tails.com>
2024-04-19 03:37:58 +0100

    Refactor config.ts: Add journals collection and update schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -15,19 +15,21 @@ const postsCollection = defineCollection({
     tags: z.array(z.string())
   })
 });
-const photographyCollection = defineCollection({
-  type: 'data',
-  schema: z.object({
+const journalsCollection = defineCollection({
+  type: 'content',
+  schema: ({ image }) => z.object({
     title: z.string(),
     pubDate: z.date(),
     description: z.string(),
-    images: z.array(z.object({
-      src: z.string(),
-      alt: z.string(),
-    })),
+    author: z.string(),
+    image: image().refine((img) => img.width >= 1080, {
+      message: "Cover image must be at least 1080 pixels wide!",
+    }),
+    imageAlt: z.string(),
+    tags: z.array(z.string())
   })
 });
 // Export a single `collections` object to register your collection(s)
 export const collections = {
-  posts: postsCollection, photographyCollection
+  posts: postsCollection, journals: journalsCollection
 };
