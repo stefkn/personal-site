8887d1a7dad7547b20d56e0a8ad5fc655dda5732
stefkn <stefan.nowak@tails.com>
2024-05-30 00:04:49 +0100

    fix site config

--- src/site-config.ts
+++ src/site-config.ts
@@ -1,13 +1,8 @@
 export type SiteConfig = {
-    logo?: Image;
     title: string;
     subtitle?: string;
     description: string;
     pageType: string;
-    image?: Image;
-    headerNavLinks?: Link[];
-    footerNavLinks?: Link[];
-    socialLinks?: Link[];
     postsPerPage?: number;
     projectsPerPage?: number;
 };
