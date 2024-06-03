5a8b7fc084a8f0b2bb3643cb20d9205594c81320
stefkn <stefan.nowak@tails.com>
2024-06-04 00:24:24 +0100

    add a technologies.js file to contain all tech stack icon data

---
+++ src/content/technologies.js
@@ -0,0 +1,212 @@
+export const programmingLanguages = [
+	{
+		name: "TypeScript",
+		icon: "skill-icons:typescript",
+	},
+	{
+		name: "JavaScript",
+		icon: "skill-icons:javascript",
+	},
+	{
+		name: "Python",
+		icon: "skill-icons:python-dark",
+	},
+	{
+		name: "Scala",
+		icon: "skill-icons:scala-dark",
+	},
+	{
+		name: "Elixir",
+		icon: "skill-icons:elixir-dark",
+	},
+	{
+		name: "Java",
+		icon: "skill-icons:java-dark",
+	},
+	{
+		name: "Go",
+		icon: "skill-icons:golang",
+	},
+	{
+		name: "RegEx",
+		icon: "skill-icons:regex-dark",
+	},
+	{
+		name: "Ruby",
+		icon: "skill-icons:ruby",
+	},
+];
+
+export const webTechnologies = [
+	{
+		name: "HTML",
+		icon: "skill-icons:html",
+	},
+	{
+		name: "CSS",
+		icon: "skill-icons:css",
+	},
+	{
+		name: "Tailwind",
+		icon: "skill-icons:tailwindcss-dark",
+	},
+	{
+		name: "Flask",
+		icon: "skill-icons:flask-dark",
+	},
+	{
+		name: "VueJS",
+		icon: "skill-icons:vuejs-dark",
+	},
+	{
+		name: "Vite",
+		icon: "skill-icons:vite-dark",
+	},
+	{
+		name: "NextJS",
+		icon: "skill-icons:nextjs-dark",
+	},
+	{
+		name: "React",
+		icon: "skill-icons:react-dark",
+	},
+	{
+		name: "Svelte",
+		icon: "skill-icons:svelte",
+	},
+	{
+		name: "Astro",
+		icon: "skill-icons:astro",
+	},
+	{
+		name: "FastAPI",
+		icon: "skill-icons:fastapi",
+	},
+	{
+		name: "Gatsby",
+		icon: "skill-icons:gatsby",
+	},
+	{
+		name: "NodeJS",
+		icon: "skill-icons:nodejs-dark",
+	},
+	{
+		name: "Nuxt",
+		icon: "skill-icons:nuxtjs-dark",
+	},
+	{
+		name: "Sass",
+		icon: "skill-icons:sass",
+	},
+];
+
+export const devopsInfra = [
+	{
+		name: "Kubernetes",
+		icon: "skill-icons:kubernetes",
+	},
+	{
+		name: "Terraform",
+		icon: "skill-icons:terraform-dark",
+	},
+	{
+		name: "AWS",
+		icon: "skill-icons:aws-dark",
+	},
+	{
+		name: "Bash",
+		icon: "skill-icons:bash-dark",
+	},
+	{
+		name: "Docker",
+		icon: "skill-icons:docker",
+	},
+	{
+		name: "GH Actions",
+		icon: "skill-icons:githubactions-dark",
+	},
+];
+
+export const databases = [
+	{
+		name: "MySQL",
+		icon: "skill-icons:mysql-dark",
+	},
+	{
+		name: "PostgreSQL",
+		icon: "skill-icons:postgresql-dark",
+	},
+	{
+		name: "MongoDB",
+		icon: "skill-icons:mongodb",
+	},
+	{
+		name: "GraphQL",
+		icon: "skill-icons:graphql-dark",
+	},
+	{
+		name: "Supabase",
+		icon: "skill-icons:supabase-dark",
+	},
+];
+
+export const testing = [
+	{
+		name: "Cypress",
+		icon: "skill-icons:cypress-light",
+	},
+	{
+		name: "Selenuim",
+		icon: "skill-icons:selenium",
+	},
+	{
+		name: "Vitest",
+		icon: "skill-icons:vitest-dark",
+	},
+	{
+		name: "Jest",
+		icon: "skill-icons:jest",
+	},
+];
+
+export const otherTechnologies = [
+    {
+        name: "TensorFlow",
+		icon: "skill-icons:tensorflow-dark",
+    },
+    {
+        name: "Markdown",
+		icon: "skill-icons:markdown-dark",
+    },
+    {
+        name: "RabbitMQ",
+		icon: "skill-icons:rabbitmq-dark",
+    },
+    {
+		name: "git",
+		icon: "skill-icons:git",
+	},
+    {
+		name: "Arduino",
+		icon: "skill-icons:arduino",
+	},
+]
+
+export const tailsStack = [
+    {
+        name: "Python",
+		icon: "skill-icons:python-dark",
+    },
+    {
+        name: "Flask",
+		icon: "skill-icons:flask-dark",
+    },
+    {
+		name: "JavaScript",
+		icon: "skill-icons:javascript",
+	},
+    {
+		name: "Go",
+		icon: "skill-icons:golang",
+	},
+]
